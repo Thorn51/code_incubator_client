848052066c3536fdd6aa287d6d9a138764f2f899
Matt Thornberry <mtberry16@hotmail.com>
2020-01-16 19:09:59 -0700

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import NavBar from "./Navigation/NavBar";
 
 function App() {
-  return <main className="App"></main>;
+  return (
+    <main className="App">
+      <NavBar login="Login" register="Register" />
+    </main>
+  );
 }
 
 export default App;
--- src/Navigation/Navbar.js
+++ src/Navigation/Navbar.js
@@ -1,8 +1,19 @@
 import React from "react";
 import "./NavBar.css";
 
-function NavBar() {
-  return <nav className="navigationBar"></nav>;
+function NavBar(props) {
+  return (
+    <nav className="navigationBar">
+      <ul className="navigation_list">
+        <li className="nav_li">
+          <a href="#registration">{props.register}</a>
+        </li>
+        <li className="nav_li">
+          <a href="#login">{props.login}</a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default NavBar;
