5b2ac65150bee1d04d3abf0f3dca62c28982dbf9
Matt Thornberry <mtberry16@hotmail.com>
2020-01-17 09:09:39 -0700

    Change to a class component

--- src/Navigation/Navbar.js
+++ src/Navigation/Navbar.js
@@ -1,19 +1,21 @@
 import React from "react";
 import "./NavBar.css";
 
-function NavBar(props) {
-  return (
-    <nav className="navigationBar">
-      <ul className="navigation_list">
-        <li className="nav_li">
-          <a href="#registration">{props.register}</a>
-        </li>
-        <li className="nav_li">
-          <a href="#login">{props.login}</a>
-        </li>
-      </ul>
-    </nav>
-  );
+class NavBar extends React.Component {
+  render() {
+    return (
+      <nav className="navigationBar">
+        <ul className="navigation_list">
+          <li className="nav_li">
+            <a href="#registration">{this.props.register}</a>
+          </li>
+          <li className="nav_li">
+            <a href="#login">{this.props.login}</a>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
 }
 
 export default NavBar;
