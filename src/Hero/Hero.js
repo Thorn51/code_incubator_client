ddb7d89de924631e1e083b6f93863f6b892df186
Matt Thornberry <mtberry16@hotmail.com>
2020-01-17 18:01:28 -0700

    Add Hero JSX

--- src/Hero/Hero.js
+++ src/Hero/Hero.js
@@ -2,5 +2,12 @@ import React from "react";
 import "./Hero.css";
 
 export default function Hero() {
-  return null;
+  return (
+    <header className="hero">
+      <h1 className="app_name">Code Incubator</h1>
+      <h3 className="hero_tag">
+        Grow your web development ideas into full-stack projects
+      </h3>
+    </header>
+  );
 }
