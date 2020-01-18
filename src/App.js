2ee38e1bad81adbc76cd450eeb6492ae9fa9f915
Matt Thornberry <mtberry16@hotmail.com>
2020-01-17 18:00:29 -0700

    Add Hero component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import NavBar from "./Navigation/NavBar";
+import Hero from "./Hero/Hero";
 
 function App() {
   return (
     <main className="App">
       <NavBar login="Login" register="Register" />
+      <Hero />
     </main>
   );
 }
