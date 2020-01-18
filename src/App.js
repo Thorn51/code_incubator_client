a42f07b7aa0e4eb95bbd0be1bde37322b2282c75
Matt Thornberry <mtberry16@hotmail.com>
2020-01-18 07:03:09 -0700

    Add CardList component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import NavBar from "./Navigation/NavBar";
 import Hero from "./Hero/Hero";
+import CardList from "./CardList/CardList";
 
-function App() {
+function App(props) {
+  console.log(props.store.ideas);
   return (
     <main className="App">
       <NavBar login="Login" register="Register" />
       <Hero />
+      <CardList ideas={props.store.ideas} />
     </main>
   );
 }
