0e759b65ab5a8585c006dc2513d532ba04ba192c
Matt Thornberry <mtberry16@hotmail.com>
2020-01-20 11:21:40 -0700

    Add JSX

--- src/Comment/Comment.js
+++ src/Comment/Comment.js
@@ -1,8 +1,25 @@
 import React from "react";
 import "./Comment.css";
+import ThumbDownIcon from "@material-ui/icons/ThumbDown";
+import ThumbUpIcon from "@material-ui/icons/ThumbUp";
 
 export default class Comment extends React.Component {
+  static defaultProps = {
+    comments: []
+  };
   render() {
-    return null;
+    const { comments } = this.props;
+    const ideaComments = comments.map(comment => {
+      <div className="comment" key={comment.id}>
+        <div className="comment_vote">
+          <ThumbUpIcon>thumb_up</ThumbUpIcon>
+          <p className="votes">{comment.votes}</p>
+          <ThumbDownIcon>thumb_down</ThumbDownIcon>
+        </div>
+        <p className="user">{comment.user_id}</p>
+        <p className="comment_text">{comment.comment_text}</p>
+      </div>;
+    });
+    return <section className="feedback">{ideaComments}</section>;
   }
 }
