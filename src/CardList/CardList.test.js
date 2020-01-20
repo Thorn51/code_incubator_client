1302b2fd24e138104d47d3549713815e08f78d37
Matt Thornberry <mtberry16@hotmail.com>
2020-01-19 17:05:05 -0700

    Create test suite

--- src/CardList/CardList.test.js
+++ src/CardList/CardList.test.js
@@ -3,13 +3,15 @@ import ReactDOM from "react-dom";
 import renderer from "react-test-renderer";
 import CardList from "./CardList";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<CardList />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe("<CardList />", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<CardList />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
 
-it("renders the UI as expected", () => {
-  const tree = renderer.create(<CardList />).toJSON();
-  expect(tree).toMatchSnapshot();
+  it("renders the UI as expected", () => {
+    const tree = renderer.create(<CardList />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
