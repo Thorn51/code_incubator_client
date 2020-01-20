e5ce29e0e1e70b53e051c96faad79a63fceca835
Matt Thornberry <mtberry16@hotmail.com>
2020-01-19 17:03:55 -0700

    Remove import

--- src/CardList/CardList.test.js
+++ src/CardList/CardList.test.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import renderer from "react-test-renderer";
 import CardList from "./CardList";
-import { ExpansionPanelActions } from "@material-ui/core";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
