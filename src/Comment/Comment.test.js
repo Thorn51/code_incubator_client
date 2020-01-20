1de47d62d8740210ad356ea0d9aa9f63ea44b243
Matt Thornberry <mtberry16@hotmail.com>
2020-01-20 08:59:25 -0700

    Setup snapshot

--- src/Comment/Comment.test.js
+++ src/Comment/Comment.test.js
@@ -1,9 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import renderer from "react-test-renderer";
 import Comment from "./Comment";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<Comment />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe("<Comment />", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<Comment />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it("renders the UI as expected", () => {
+    const tree = renderer.create(<Comment />).toJSON();
+    expect("tree").toMatchSnapshot();
+  });
 });
