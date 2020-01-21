d8a9fa8b67c7951aa0c1e823a0a9fca3fe18355e
Matt Thornberry <mtberry16@hotmail.com>
2020-01-20 19:38:53 -0700

    Setup routes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import HomePage from "./routes/HomePage/HomePage";
 import LoginPage from "./routes/LoginPage/LoginPage";
 import RegistrationPage from "./routes/RegistrationPage/RegistrationPage";
 import NotFoundPage from "./routes/NoteFoundPage/NoteFoundPage";
+import IdeaPage from "./routes/IdeaPage/IdeaPage";
+import SubmitIdeaPage from "./routes/SubmitIdeaPage/SubmitIdeaPage";
 
 class App extends React.Component {
   static defaultProps = {
@@ -18,9 +20,11 @@ class App extends React.Component {
     return (
       <main className="App">
         <Switch>
-          <Route exact path="/" component={HomePage} store={store} />
+          <Route exact path="/" render={props => <HomePage {...store} />} />
+          <Route path="/idea/:ideaId" component={IdeaPage} />
           <Route path="/login" component={LoginPage} />
           <Route path="/register" component={RegistrationPage} />
+          <Route path="/submitidea" component={SubmitIdeaPage} />
           <Route component={NotFoundPage} />
         </Switch>
       </main>
