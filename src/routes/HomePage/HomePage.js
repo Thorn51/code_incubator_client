9690254a98966773aa490d7cb5f55a9945a9300e
Matt Thornberry <mtberry16@hotmail.com>
2020-01-21 09:39:54 -0700

    add submit idea button

--- src/routes/HomePage/HomePage.js
+++ src/routes/HomePage/HomePage.js
@@ -1,17 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import NavBar from "../../components/Navigation/NavBar";
 import Hero from "../../components/Hero/Hero";
 import CardList from "../../components/CardList/CardList";
 import Footer from "../../components/Footer/Footer";
+import "./HomePage.css";
 
 class HomePage extends React.Component {
   render() {
-    const { ideas } = this.props;
+    const { ideas, users } = this.props;
     return (
       <main className="App">
-        <NavBar login="Login" register="Register" />
+        <NavBar />
         <Hero />
-        <CardList ideas={ideas} />
+        <Link to="/submitidea">
+          <button type="button" className="idea_button">
+            Submit Idea
+          </button>
+        </Link>
+        <CardList ideas={ideas} users={users} />
         <Footer />
       </main>
     );
