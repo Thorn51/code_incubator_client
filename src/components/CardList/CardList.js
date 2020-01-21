a0ef299aa4d1992fb8ac3cee3fd97d36641eeee2
Matt Thornberry <mtberry16@hotmail.com>
2020-01-20 19:39:40 -0700

    Add links to cards

--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./CardList.css";
 import ThumbDownIcon from "@material-ui/icons/ThumbDown";
 import ThumbUpIcon from "@material-ui/icons/ThumbUp";
@@ -10,19 +11,21 @@ export default class CardList extends React.Component {
   render() {
     const { ideas } = this.props;
     const card = ideas.map(idea => (
-      <div className="idea_card" key={idea.id}>
-        <div className="project_info">
-          <p>{idea.user_id}</p>
-          <p>{idea.date_submitted}</p>
+      <Link to={`/idea/${idea.id}`} key={idea.id}>
+        <div className="idea_card">
+          <div className="project_info">
+            <p>{idea.user_id}</p>
+            <p>{idea.date_submitted}</p>
+          </div>
+          <h3 className="card_title">{idea.project_title}</h3>
+          <p className="card_content">{truncate(idea.project_summary)}</p>
+          <div className="card_vote">
+            <ThumbUpIcon>thumb_down</ThumbUpIcon>
+            <p className="votes_for">{idea.votes}</p>
+            <ThumbDownIcon>thumb_down</ThumbDownIcon>
+          </div>
         </div>
-        <h3 className="card_title">{idea.project_title}</h3>
-        <p className="card_content">{truncate(idea.project_summary)}</p>
-        <div className="card_vote">
-          <ThumbUpIcon>thumb_down</ThumbUpIcon>
-          <p className="votes_for">{idea.votes}</p>
-          <ThumbDownIcon>thumb_down</ThumbDownIcon>
-        </div>
-      </div>
+      </Link>
     ));
     return <div className="card_container">{card}</div>;
   }
