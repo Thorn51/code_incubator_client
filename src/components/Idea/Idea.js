8cdedda188b78342bf5fe099c302b9c113663c36
Matt Thornberry <mtberry16@hotmail.com>
2020-01-23 13:00:42 -0700

    Setup component to handle idea details

---
+++ src/components/Idea/Idea.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./Idea.css";
+import ThumbDownIcon from "@material-ui/icons/ThumbDown";
+import ThumbUpIcon from "@material-ui/icons/ThumbUp";
+
+export default function Idea(props) {
+  return (
+    <>
+      <header className="idea_page_header">
+        <h1 className="app_name">{props.idea.project_title}</h1>
+        <div className="project_vote">
+          <ThumbUpIcon onClick={() => props.projectUpVote(props.idea.id)}>
+            thumb_up
+          </ThumbUpIcon>
+          <p className="votes_for">{props.idea.votes}</p>
+          <ThumbDownIcon onClick={() => props.projectDownVote(props.idea.id)}>
+            thumb_down
+          </ThumbDownIcon>
+        </div>
+      </header>
+      <section className="project_details">
+        <span className="project_author">
+          Submitted by: {props.author.nick_name}
+        </span>
+        <span className="date_submitted">
+          Submitted: {props.idea.date_submitted}
+        </span>
+        <span className="project_status">Status: {props.idea.status}</span>
+        <span className="repo">Repo: {props.idea.github}</span>
+      </section>
+
+      <section className="idea_description">
+        <h2 className="section_title">Overview</h2>
+        <p>{props.idea.project_summary}</p>
+      </section>
+    </>
+  );
+}
