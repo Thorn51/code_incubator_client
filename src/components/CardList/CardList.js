14b779865cf0d942cc5f93f31d32edc49214f3bc
Matt Thornberry <mtberry16@hotmail.com>
2020-01-23 12:59:26 -0700

    Refact to functional component

--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -4,36 +4,29 @@ import "./CardList.css";
 import ThumbDownIcon from "@material-ui/icons/ThumbDown";
 import ThumbUpIcon from "@material-ui/icons/ThumbUp";
 
-export default class CardList extends React.Component {
-  static defaultProps = {
-    ideas: [],
-    users: []
-  };
-  render() {
-    const { ideas, users } = this.props;
-
-    const card = ideas.map(idea => {
-      const author = users.find(author => author.id === idea.user_id);
-      return (
-        <Link to={`/idea/${idea.id}`} key={idea.id} className="card_link">
-          <div className="idea_card">
-            <div className="project_info">
-              <p>{author.nick_name}</p>
-              <p>{idea.date_submitted}</p>
-            </div>
-            <h3 className="card_title">{idea.project_title}</h3>
-            <p className="card_content">{truncate(idea.project_summary)}</p>
-            <div className="card_vote">
-              <ThumbUpIcon>thumb_down</ThumbUpIcon>
-              <p className="votes_for">{idea.votes}</p>
-              <ThumbDownIcon>thumb_down</ThumbDownIcon>
-            </div>
+export default function CardList(props) {
+  const card = props.ideas.map(idea => {
+    const author = props.users.find(author => author.id === idea.user_id);
+    console.log(idea.project_summary);
+    return (
+      <Link to={`/idea/${idea.id}`} key={idea.id} className="card_link">
+        <div className="idea_card">
+          <div className="project_info">
+            <p>{author.nick_name}</p>
+            <p>{idea.date_submitted}</p>
           </div>
-        </Link>
-      );
-    });
-    return <div className="card_container">{card}</div>;
-  }
+          <h3 className="card_title">{idea.project_title}</h3>
+          <p className="card_content">{truncate(idea.project_summary)}</p>
+          <div className="card_vote">
+            <ThumbUpIcon>thumb_down</ThumbUpIcon>
+            <p className="votes_for">{idea.votes}</p>
+            <ThumbDownIcon>thumb_down</ThumbDownIcon>
+          </div>
+        </div>
+      </Link>
+    );
+  });
+  return <div className="card_container">{card}</div>;
 }
 
 function truncate(content) {
@@ -41,5 +34,7 @@ function truncate(content) {
 
   if (words.length > 15) {
     return words.slice(0, 20).join(" ") + "...";
+  } else {
+    return words.join(" ");
   }
 }
