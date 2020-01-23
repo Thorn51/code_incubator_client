6d9e4693d142c6409828d323ad3eafd1c48b011e
Matt Thornberry <mtberry16@hotmail.com>
2020-01-23 07:34:59 -0700

    Refactor to single source of truth

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -3,47 +3,20 @@ import "./Comment.css";
 import ThumbDownIcon from "@material-ui/icons/ThumbDown";
 import ThumbUpIcon from "@material-ui/icons/ThumbUp";
 
-export default class Comment extends React.Component {
-  static defaultProps = {
-    comments: []
-  };
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      votes: parseInt(props.votes)
-    };
-  }
-
-  handleThumbsUp = () => {
-    const votes = this.state.votes + 1;
-    this.setState({
-      votes
-    });
-  };
-
-  handleThumbsDown = () => {
-    const votes = this.state.votes - 1;
-    this.setState({
-      votes
-    });
-  };
-
-  commentAuthor = author => {};
-
-  render() {
-    return (
-      <div className="comment">
-        <div className="comment_vote">
-          <ThumbUpIcon onClick={this.handleThumbsUp}>thumb_up</ThumbUpIcon>
-          <p className="votes">{this.state.votes}</p>
-          <ThumbDownIcon onClick={this.handleThumbsDown}>
-            thumb_down
-          </ThumbDownIcon>
-        </div>
-        <p className="user">{this.props.author}</p>
-        <p className="comment_text">{this.props.content}</p>
+export default function Comment(props) {
+  return (
+    <div className="comment">
+      <div className="comment_vote">
+        <ThumbUpIcon onClick={() => props.commentUpVote(props.id)}>
+          thumb_up
+        </ThumbUpIcon>
+        <p className="votes">{props.votes}</p>
+        <ThumbDownIcon onClick={() => props.commentDownVote(props.id)}>
+          thumb_down
+        </ThumbDownIcon>
       </div>
-    );
-  }
+      <p className="user">{props.author}</p>
+      <p className="comment_text">{props.content}</p>
+    </div>
+  );
 }
