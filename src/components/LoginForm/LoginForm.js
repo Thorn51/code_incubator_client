b0e00895d030bbf9cb1667f6755595b7a04a8924
Matt Thornberry <mtberry16@hotmail.com>
2020-02-04 17:29:19 -0700

    Add handler to submit login credentials

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,26 +1,41 @@
 import React from "react";
+import TokenServices from "../../services/token-service";
 import "./LoginForm.css";
 
 export default class LoginForm extends React.Component {
+  handleLoginBasicAuth = e => {
+    e.preventDefault();
+    const { email, password } = e.target;
+
+    console.log(email.value, password.value);
+
+    TokenServices.saveAuthToken(
+      TokenServices.makeBasicAuthToken(email.value, password.value)
+    );
+
+    email.value = "";
+    password.value = "";
+    this.props.onLoginSuccess();
+  };
   render() {
     return (
-      <form className="registration_form">
+      <form className="registration_form" onSubmit={this.handleLoginBasicAuth}>
         <h2 className="section_title">Login</h2>
         <hr />
-        <label htmlFor="login_email">Email:</label>
+        <label htmlFor="email">Email:</label>
         <input
           type="text"
           placeholder="Enter Email"
           name="email"
-          id="login_email"
+          id="email"
           required
         />
-        <label htmlFor="login_password">Password:</label>
+        <label htmlFor="password">Password:</label>
         <input
           type="password"
           placeholder="Enter Password"
           name="password"
-          id="login_password"
+          id="password"
           required
         />
         <button type="submit" className="form_button">
