54177df4d5f50e82beb74abb2b54471d720de7c2
Matt Thornberry <mtberry16@hotmail.com>
2020-02-04 19:04:30 -0700

    Setup basic auth

--- src/routes/LoginPage/LoginPage.js
+++ src/routes/LoginPage/LoginPage.js
@@ -5,20 +5,35 @@ import Hero from "../../components/Hero/Hero";
 import LoginForm from "../../components/LoginForm/LoginForm";
 import Footer from "../../components/Footer/Footer";
 
-export default function LoginPage() {
-  return (
-    <div>
-      <NavBar />
-      <Hero />
-      <LoginForm />
-      <div className="form_redirect">
-        <hr />
-        <p>
-          Do you need to create an account? Visit the{" "}
-          <Link to="/register">registration page</Link>.
-        </p>
+export default class LoginPage extends React.Component {
+  static defaultProps = {
+    location: {},
+    history: {
+      push: () => {}
+    }
+  };
+
+  handleLoginSuccess = () => {
+    const { location, history } = this.props;
+    const destination = (location.state || {}).from || "/";
+    history.push(destination);
+  };
+
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <Hero />
+        <LoginForm onLoginSuccess={this.handleLoginSuccess} />
+        <div className="form_redirect">
+          <hr />
+          <p>
+            Do you need to create an account? Visit the{" "}
+            <Link to="/register">registration page</Link>.
+          </p>
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
-  );
+    );
+  }
 }
