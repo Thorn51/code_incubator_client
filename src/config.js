c84ffeddbefede78a3ffa820284286974d786040
Matt Thornberry <mtberry16@hotmail.com>
2020-02-04 17:28:50 -0700

    Remove unecessary code

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
   API_ENDPOINT: "http://localhost:8000",
-  API_TOKEN: process.env.REACT_APP_API_KEY
+  API_TOKEN: process.env.REACT_APP_API_KEY,
+  TOKEN_KEY: "incubator-client-auth-token"
 };
