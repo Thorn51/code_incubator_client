549896117fe4d9cd31f33d01433e7dcf60827a06
Matt Thornberry <mtberry16@hotmail.com>
2020-02-06 10:02:04 -0700

    Initial commit

---
+++ src/components/utils/PrivateRoute.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import TokenServices from "../../services/token-service";
+
+export default function PrivateRoute({ component, ...props }) {}
