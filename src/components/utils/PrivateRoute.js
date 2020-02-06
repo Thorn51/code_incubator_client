29997ddaf5c6362eaa83b3da1ced74421fe946ad
Matt Thornberry <mtberry16@hotmail.com>
2020-02-06 10:45:06 -0700

    Setup private route

--- src/components/utils/PrivateRoute.js
+++ src/components/utils/PrivateRoute.js
@@ -2,4 +2,23 @@ import React from "react";
 import { Route, Redirect } from "react-router-dom";
 import TokenServices from "../../services/token-service";
 
-export default function PrivateRoute({ component, ...props }) {}
+export default function PrivateRoute({ component, ...props }) {
+  const Component = component;
+  return (
+    <Route
+      {...props}
+      render={componentProps =>
+        TokenServices.hasAuthToken() ? (
+          <Component {...componentProps} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: componentProps.location }
+            }}
+          />
+        )
+      }
+    />
+  );
+}
