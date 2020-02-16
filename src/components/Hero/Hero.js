c3cb9de359112545406ccdac0eecb40e8b461b74
Matt Thornberry <mtberry16@hotmail.com>
2020-02-16 10:01:34 -0700

    Move navbar

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./Hero.css";
+import NavBar from "../Navigation/NavBar";
 
 export default function Hero() {
   return (
     <header className="hero">
+      <NavBar />
       <h1 className="app_name">Code Incubator</h1>
       <h3 className="hero_tag">
         Grow your web development ideas into full-stack projects
