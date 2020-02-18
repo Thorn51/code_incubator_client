e375162972f9cc8a7865842a7e2f3e05dc94ec19
Matt Thornberry <mtberry16@hotmail.com>
2020-02-17 19:26:17 -0700

    Add browser router

--- src/components/Hero/Hero.test.js
+++ src/components/Hero/Hero.test.js
@@ -1,17 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import renderer from "react-test-renderer";
 import Hero from "./Hero";
 
 describe("<Hero />", () => {
-  it("renders without crasing", () => {
+  it("renders without crashing", () => {
     const div = document.createElement("div");
-    ReactDOM.render(<Hero />, div);
+    ReactDOM.render(
+      <BrowserRouter>
+        <Hero />
+      </BrowserRouter>,
+      div
+    );
     ReactDOM.unmountComponentAtNode(div);
   });
 
   it("Renders HERO as expected", () => {
-    const tree = renderer.create(<Hero />).toJSON();
+    const tree = renderer
+      .create(
+        <BrowserRouter>
+          <Hero />
+        </BrowserRouter>
+      )
+      .toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
