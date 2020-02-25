d8cc84a5c62930e7fa25c0250859bde660d9bd5d
Matt Thornberry <mtberry16@hotmail.com>
2020-02-25 07:55:40 -0700

    Refactor to add vote logic

--- src/routes/IdeaPage/IdeaPage.js
+++ src/routes/IdeaPage/IdeaPage.js
@@ -1,17 +1,21 @@
 import React from "react";
 import "./IdeaPage.css";
 import NavBar from "../../components/Navigation/NavBar";
-import Idea from "../../components/Idea/Idea";
 import Footer from "../../components/Footer/Footer";
 import ApiService from "../../services/api-service";
-import CommentList from "../../components/commentList/CommentList";
+import CommentList from "../../components/CommentList/CommentList";
+import ThumbDownIcon from "@material-ui/icons/ThumbDown";
+import ThumbUpIcon from "@material-ui/icons/ThumbUp";
+import moment from "moment";
+import TokenServices from "../../services/token-service";
 
 export default class IdeaPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       idea: [],
-      ideaAuthor: []
+      ideaAuthor: [],
+      ideaVotes: []
     };
   }
 
@@ -32,6 +36,14 @@ export default class IdeaPage extends React.Component {
             ideaAuthor
           });
         });
+      })
+      .then(() => {
+        ApiService.getAllIdeaVotes().then(data => {
+          let ideaVotes = data.filter(vote => vote.idea === this.state.idea.id);
+          this.setState({
+            ideaVotes
+          });
+        });
       });
   }
 
@@ -43,6 +55,17 @@ export default class IdeaPage extends React.Component {
     ApiService.patchProjectVote(votes, this.state.idea.id).then(() => {
       this.setState({
         idea
+      });
+    });
+
+    ApiService.postIdeaVote(1, idea.id).then(() => {
+      ApiService.getAllIdeaVotes().then(() => {
+        ApiService.getAllIdeaVotes().then(data => {
+          let ideaVotes = data.filter(vote => vote.idea === this.state.idea.id);
+          this.setState({
+            ideaVotes
+          });
+        });
       });
     });
   };
@@ -57,22 +80,103 @@ export default class IdeaPage extends React.Component {
         idea
       });
     });
+
+    ApiService.postIdeaVote(-1, idea.id).then(() => {
+      ApiService.getAllIdeaVotes().then(() => {
+        ApiService.getAllIdeaVotes().then(data => {
+          let ideaVotes = data.filter(vote => vote.idea === this.state.idea.id);
+          this.setState({
+            ideaVotes
+          });
+        });
+      });
+    });
   };
+
+  renderVoteOptions() {
+    const token = TokenServices.getAuthToken();
+    const { user_id } = TokenServices.getUserDetails(token);
+
+    let usersInVotes = [];
+
+    this.state.ideaVotes.forEach(vote => usersInVotes.push(vote.vote_by_user));
+
+    const userPlacedVote = usersInVotes.includes(user_id);
+
+    let loggedInUserVote;
+
+    if (userPlacedVote === true) {
+      loggedInUserVote = this.state.ideaVotes.find(
+        vote => vote.vote_by_user === user_id
+      );
+    }
+
+    if (userPlacedVote === false) {
+      return (
+        <div className="project_vote">
+          <ThumbUpIcon onClick={this.projectUpVote}>thumb_up</ThumbUpIcon>
+          <p className="votes_for">{this.state.idea.votes}</p>
+          <ThumbDownIcon onClick={this.projectDownVote}></ThumbDownIcon>
+        </div>
+      );
+    } else if (userPlacedVote === true && loggedInUserVote.vote === "1") {
+      return (
+        <div className="project_vote">
+          <ThumbUpIcon className="green"></ThumbUpIcon>
+          <p className="votes_for">
+            {this.state.idea.votes} People say go for it!
+          </p>
+          <ThumbDownIcon className="disabled"></ThumbDownIcon>
+        </div>
+      );
+    } else if (userPlacedVote === true && loggedInUserVote.vote === "-1") {
+      return (
+        <div className="project_vote">
+          <ThumbUpIcon className="disabled"></ThumbUpIcon>
+          <p className="votes_for">
+            {this.state.idea.votes} People say go for it!
+          </p>
+          <ThumbDownIcon className="red"></ThumbDownIcon>
+        </div>
+      );
+    }
+  }
 
   render() {
     return (
       <div>
         <NavBar />
-        <Idea
-          idea={this.state.idea}
-          author={this.state.ideaAuthor}
-          projectUpVote={this.projectUpVote}
-          projectDownVote={this.projectDownVote}
-        />
-        <section className="feedback">
-          <h2 className="section_title">Feedback</h2>
-          <CommentList project={this.props.match.params.id} />
+        <header className="idea_page_header">
+          <h1 className="app_name">{this.state.idea.project_title}</h1>
+          {this.renderVoteOptions()}
+        </header>
+        <section className="project_details">
+          <p>
+            <span className="bold"> Submitted by:</span>{" "}
+            <span>{this.state.ideaAuthor.nickname}</span>
+          </p>
+          <p>
+            <span className="bold">Submit Date:</span>{" "}
+            <span>
+              {moment(this.state.idea.date_submitted).format("MMM Do YYYY")}
+            </span>
+          </p>
+          <p>
+            <span className="bold">Status:</span>{" "}
+            <span>{this.state.idea.status}</span>
+          </p>
+          {this.state.idea.github === "" ? null : (
+            <p>
+              <span className="bold">Repo:</span>{" "}
+              <span>{this.state.idea.github}</span>
+            </p>
+          )}
         </section>
+        <section className="idea_description">
+          <h2 className="section_title">Overview</h2>
+          <p>{this.state.idea.project_summary}</p>
+        </section>
+        <CommentList project={this.props.match.params.id} />
         <Footer />
       </div>
     );
