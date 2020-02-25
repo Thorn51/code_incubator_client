8cf43ddb987aba94c87330b452165bb43ab6b13b
Matt Thornberry <mtberry16@hotmail.com>
2020-02-25 07:48:38 -0700

    Setup voting options logic for rendering

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -4,11 +4,12 @@ import ThumbDownIcon from "@material-ui/icons/ThumbDown";
 import ThumbUpIcon from "@material-ui/icons/ThumbUp";
 import moment from "moment";
 import ApiService from "../../services/api-service";
+import TokenServices from "../../services/token-service";
 
 export default class Comment extends React.Component {
   state = {
     comment: [],
-    userVote: false,
+    commentVotes: [],
     ready: false
   };
 
@@ -17,6 +18,16 @@ export default class Comment extends React.Component {
       .then(comment => {
         this.setState({
           comment
+        });
+      })
+      .then(() => {
+        ApiService.getAllCommentVotes().then(votes => {
+          const commentVotes = votes.filter(
+            vote => vote.comment === this.state.comment.id
+          );
+          this.setState({
+            commentVotes
+          });
         });
       })
       .then(() => {
@@ -35,21 +46,86 @@ export default class Comment extends React.Component {
   }
 
   upVote = () => {
-    console.log(this.state.comment.id, "Up Vote");
+    let comment = this.state.comment;
+    comment.votes = parseInt(comment.votes) + 1;
+    ApiService.patchCommentVote(comment.votes, comment.id).then(() => {
+      this.setState({
+        comment
+      });
+    });
+    ApiService.postCommentVote(1, comment.id).then(data => {
+      this.setState({
+        commentVotes: [...this.state.commentVotes, data]
+      });
+    });
   };
 
   downVote = () => {
-    console.log(this.state.comment.id, "Down Vote");
+    let comment = this.state.comment;
+    comment.votes = parseInt(comment.votes) - 1;
+    ApiService.patchCommentVote(comment.votes, comment.id).then(() => {
+      this.setState({
+        comment
+      });
+    });
+    ApiService.postCommentVote(-1, comment.id).then(data => {
+      this.setState({
+        commentVotes: [...this.state.commentVotes, data]
+      });
+    });
   };
 
-  render() {
-    return (
-      <div className="comment">
+  renderVoteOptions() {
+    const token = TokenServices.getAuthToken();
+    const { user_id } = TokenServices.getUserDetails(token);
+
+    let usersInVotes = [];
+
+    this.state.commentVotes.forEach(vote =>
+      usersInVotes.push(vote.vote_by_user)
+    );
+
+    const userPlacedVote = usersInVotes.includes(user_id);
+
+    let loggedInUserVote;
+
+    if (userPlacedVote === true) {
+      loggedInUserVote = this.state.commentVotes.find(
+        vote => vote.vote_by_user === user_id
+      );
+    }
+
+    if (userPlacedVote === false) {
+      return (
         <div className="comment_vote">
           <ThumbUpIcon onClick={this.upVote}>thumb_up</ThumbUpIcon>
           <p className="votes">{this.state.comment.votes}</p>
           <ThumbDownIcon onClick={this.downVote}>thumb_down</ThumbDownIcon>
         </div>
+      );
+    } else if (userPlacedVote === true && loggedInUserVote.vote === "1") {
+      return (
+        <div className="comment_vote">
+          <ThumbUpIcon className="green"></ThumbUpIcon>
+          <p className="votes">{this.state.comment.votes} Thumbs Up!</p>
+          <ThumbDownIcon className="disabled"></ThumbDownIcon>
+        </div>
+      );
+    } else if (userPlacedVote === true && loggedInUserVote.vote === "-1") {
+      return (
+        <div className="comment_vote">
+          <ThumbUpIcon className="disabled"></ThumbUpIcon>
+          <p className="votes">{this.state.comment.votes} Thumbs Up!</p>
+          <ThumbDownIcon className="red"></ThumbDownIcon>
+        </div>
+      );
+    }
+  }
+
+  render() {
+    return (
+      <div className="comment">
+        {this.state.ready && this.renderVoteOptions()}
         <div className="comment_header">
           <p className="user">
             {this.state.ready && this.state.author.nickname}
